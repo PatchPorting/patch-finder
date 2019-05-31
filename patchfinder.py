5d695adc561072159af206c3fb9b5e5fe237159b
Luciano Bello <luciano.bello@ibm.com>
2019-05-31 10:32:20 -0400

    cve example

---
+++ patchfinder.py
@@ -0,0 +1,10 @@
+import argparse
+from patchfinder.vulnerability import CVE
+
+parser = argparse.ArgumentParser(description='Finds patches for a vulnerability id.')
+parser.add_argument('--cve')
+
+args = parser.parse_args()
+cve = CVE(args.cve)
+for entrypoint in cve.entrypoint_URLs():
+    print(entrypoint)
