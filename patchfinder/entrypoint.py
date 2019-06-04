0204263f61b3358abc48a10d5c8248631a9941e5
jajajasalu2 <8iamthewalrus8@gmail.com>
2019-06-04 14:42:53 +0530

    Extended on Entrypoint
    Added a child class Provider to Entrypoint and defined a link matching method for Provider. Created equivalent test

--- patchfinder/entrypoint.py
+++ patchfinder/entrypoint.py
@@ -1,28 +1,61 @@
 #TODO: figure out a more sophisticated way to construct URLs,
 #      maybe define a method to substitute a %s for multiple strings
-#NOTE: Might require an additional Patch Provider class
 
 class Entrypoint(object):
     """Base class for an Entrypoint
+
+    Entrypoint is an entity crawled by the patch-finder.
+    It can be understood as a seed in the crawling process.
 
     Attributes:
         urls: The entry urls to crawl
     """
 
     def __init__(self, urls):
+        """init method"""
         self.urls = urls
 
 
-class Github(Entrypoint):
-    """Subclass for GitHub as an entrypoint"""
+class Provider(Entrypoint):
+    """Subclass for a patch Provider
 
-    def __init__(self, vuln_id):
-        urls = ['https://github.com/search?q='+vuln_id+'&type=Commits']
-        super(Github, self).__init__(urls)
+    Inherits from Entrypoint
+    Provider is an entity that provides patches.
+    A provider can also be an Entrypoint.
+    """
+
+    def __init__(self, link_components, urls):
+        """init method, calls init of Entrypoint if urls is not None
+
+        Args:
+            link_components: A list of components in a patch link for this provider
+            urls: A list of URLs the finder will crawl w/r/t this provider
+        """
+        if urls:
+            super(Provider, self).__init__(urls)
+        self.link_components = link_components
+
+    def match_link(self, link):
+        """Checks if 'link' belongs to this provider"""
+        if all(x in link for x in self.link_components):
+            return True
+        return False
+
+
+class Github(Provider):
+    """Subclass for GitHub as a Provider"""
+
+    def __init__(self, vuln_id=None):
+        if vuln_id:
+            urls = ['https://github.com/search?q='+vuln_id+'&type=Commits']
+        else:
+            urls = None
+        link_components = ['github.com', '/commit/']
+        super(Github, self).__init__(link_components, urls)
 
 
 class NVD(Entrypoint):
-    """Subclass for nvd.nist.org as an entrpoint"""
+    """Subclass for nvd.nist.org as an entrypoint"""
 
     def __init__(self, vuln_id):
         urls = ['https://nvd.nist.org/vuln/details/'+vuln_id]
--- tests/test_entrypoint.py
+++ tests/test_entrypoint.py
@@ -1,5 +1,6 @@
 import unittest
 import patchfinder.context as context
+import patchfinder.entrypoint as entrypoint
 
 class TestEntrypoint(unittest.TestCase):
     """Test Class for Entrypoint"""
@@ -10,7 +11,18 @@ class TestEntrypoint(unittest.TestCase):
         self.assertEqual(entrypoints[0].urls,
                          ['https://nvd.nist.org/vuln/details/CVE-2018-20406'])
         self.assertEqual(entrypoints[1].urls,
-                         ['https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2018-20406'])
+                         ['https://cve.mitre.org/cgi-bin/cvename.cgi?' \
+                          'name=CVE-2018-20406'])
+
+    def test_github(self):
+        github = entrypoint.Github()
+        link = 'https://github.com/python/cpython/commit/a4ae828ee416a6' \
+                '6d8c7bf5ee71d653c2cc6a26dd'
+        self.assertEqual(github.link_components, ['github.com', '/commit/'])
+        github = entrypoint.Github('CVE-2018-20406')
+        self.assertEqual(github.urls, ['https://github.com/search?q=CVE-2018-' \
+                                      '20406&type=Commits'])
+        self.assertEqual(github.link_components, ['github.com', '/commit/'])
 
 
 if __name__ == '__main__':
