2d0ade38759ab87c8155c872570f08f831f13039
jajajasalu2 <8iamthewalrus8@gmail.com>
2019-06-14 17:21:09 +0530

    Progress on DebianParser, wrote more tests

--- patchfinder/debian.py
+++ patchfinder/debian.py
@@ -1,7 +1,8 @@
 import os
 import re
-import scrapy
 import urllib.request
+from bs4 import BeautifulSoup
+DOWNLOAD_DIRECTORY = './cache/'
 
 class DebianParser(object):
     """Class for parsing utilities relevant to Debian
@@ -9,39 +10,65 @@ class DebianParser(object):
     Attributes:
         cve_list_url: The link to the CVE list used in the Deb Sec Tracker
         cve_file: The path to the file used to save the cve list locally
+        fixed_packages: A list of fixed Debian packages w/r/t the input
+            vulnerability
     """
 
     cve_list_url = 'https://salsa.debian.org/security-tracker-team/security' \
                 '-tracker/raw/master/data/CVE/list'
-    cve_file = './cache/list'
+    cve_file = DOWNLOAD_DIRECTORY + 'list'
+    fixed_packages = []
 
-    def _download_cve_list(self):
-        if os.path.isfile(self.cve_file):
+    #TODO: urlretrieve is most probably deprecated, find something else
+    def _download_item(self, url, save_as):
+        if os.path.isfile(save_as):
             return
-        urllib.request.urlretrieve(self.cve_list_url, self.cve_file)
+        urllib.request.urlretrieve(url, save_as)
 
-    def fixed_packages(self):
-        self._download_cve_list()
+    def find_fixed_packages(self):
+        self.fixed_packages = []
+        self._download_item(self.cve_list_url, self.cve_file)
         vuln_found = 0
-        fixed_packages = []
         look_for_cve = re.compile(r'^{vuln_id}'.format(vuln_id=self.vuln_id))
-        pkg_ver = re.compile(r'^\t\- (.+?) ([a-zA-Z\d\.\+\-\~:]+?$|[a-zA-Z\d\.' \
-                             '\+\-\~:]+? )')
-        f = open(self.cve_file)
-        for line in f:
-            if vuln_found:
-                if re.match(r'^CVE', line):
-                    break
-                matches = pkg_ver.search(line)
-                if matches and len(matches.groups()) is 2:
-                    fixed_packages.append({'package': matches.group(1).strip(),
-                                           'version': matches.group(2).strip()})
-            elif look_for_cve.match(line):
-                vuln_found = 1
-        f.close()
-        return fixed_packages
+        pkg_ver = re.compile(r'^\t\- (.+?) ([a-zA-Z\d\.\+\-\~:]+?$|[a-zA-Z' \
+                             '\d\.\+\-\~:]+? )')
+        cve_file = open(self.cve_file)
+        try:
+            for line in cve_file:
+                if vuln_found:
+                    if re.match(r'^CVE', line):
+                        break
+                    matches = pkg_ver.search(line)
+                    if matches and len(matches.groups()) is 2:
+                        pkg = matches.group(1).strip()
+                        ver = re.sub('^.+:', '', matches.group(2).strip())
+                        self.fixed_packages.append({'package': pkg,
+                                                    'version': ver})
+                elif look_for_cve.match(line):
+                    vuln_found = 1
+        finally:
+            cve_file.close()
+
+    def retrieve_packages(self):
+        for package in self.fixed_packages:
+            snapshot_url = 'https://snapshot.debian.org/package/{pkg}/{ver}/' \
+                    .format(pkg=package['package'], ver=package['version'])
+            snapshot_html = urllib.request.urlopen(snapshot_url)
+            soup = BeautifulSoup(snapshot_html, 'html.parser')
+            find_pkg = re.compile(r'/({pkg}_{ver}\.(debian\.tar\..+|diff\..+' \
+                                  '))$'.format(pkg=package['package'],
+                                               ver=package['version']))
+            pkg_url = soup.find('a', href=find_pkg)
+            assert pkg_url, "Couldn't find package {pkg} {ver} on {url}" \
+                    .format(pkg=package['package'],
+                            ver=package['version'],
+                            url=snapshot_url)
+            pkg_url = 'https://snapshot.debian.org/' + pkg_url['href']
+            pkg_name = find_pkg.search(pkg_url)
+            self._download_item(pkg_url, DOWNLOAD_DIRECTORY + pkg_name.group(1))
 
     def parse(self, vuln_id):
         self.vuln_id = vuln_id
-        packages = self.fixed_packages()
+        self.find_fixed_packages()
+        self.retrieve_packages()
         return []
--- tests/test_debian_parse.py
+++ tests/test_debian_parse.py
@@ -1,5 +1,7 @@
+import os.path
 import unittest
 from patchfinder.debian import DebianParser
+#TODO: Create more tests
 
 class TestDebianParser(unittest.TestCase):
     """Test Class for DebianParser"""
@@ -7,17 +9,30 @@ class TestDebianParser(unittest.TestCase):
     def test_fixed_packages(self):
         debian_parser = DebianParser()
         debian_parser.vuln_id = 'CVE-2019-12795'
-        pkgs = debian_parser.fixed_packages()
-        self.assertTrue({'package':'gvfs', 'version': '1.38.1-5'} in pkgs)
-        self.assertTrue(len(pkgs) is 1)
+        debian_parser.find_fixed_packages()
+        self.assertTrue({'package':'gvfs', 'version': '1.38.1-5'} in debian_parser.fixed_packages)
+        self.assertTrue(len(debian_parser.fixed_packages) is 1)
         debian_parser.vuln_id = 'CVE-2016-10739'
-        pkgs = debian_parser.fixed_packages()
-        self.assertTrue({'package': 'glibc', 'version': '2.28-6'} in pkgs)
-        self.assertTrue(len(pkgs) is 1)
+        debian_parser.find_fixed_packages()
+        self.assertTrue({'package': 'glibc', 'version': '2.28-6'} in debian_parser.fixed_packages)
+        self.assertTrue(len(debian_parser.fixed_packages) is 1)
         debian_parser.vuln_id = 'CVE-2004-2779'
-        pkgs = debian_parser.fixed_packages()
-        self.assertTrue({'package': 'libid3tag', 'version': '0.15.1b-5'} in pkgs)
-        self.assertTrue(len(pkgs) is 1)
+        debian_parser.find_fixed_packages()
+        self.assertTrue({'package': 'libid3tag', 'version': '0.15.1b-5'} in debian_parser.fixed_packages)
+        self.assertTrue(len(debian_parser.fixed_packages) is 1)
+
+    def test_debian_parse(self):
+        debian_parser = DebianParser()
+        pkg_name = 'openjpeg2_2.1.1-1.debian.tar.xz'
+        debian_parser.parse('CVE-2016-4796')
+        self.assertTrue(os.path.isfile('./cache/' + pkg_name))
+        debian_parser.parse('CVE-2004-2779')
+        pkg_name = 'libid3tag_0.15.1b-5.diff.gz'
+        self.assertTrue(os.path.isfile('./cache/' + pkg_name))
+
+    def test_cve_file_name(self):
+        debian_parser = DebianParser()
+        self.assertEqual(debian_parser.cve_file, './cache/list')
 
 if __name__ == '__main__':
     unittest.main()
