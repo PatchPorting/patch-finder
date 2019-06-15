1ba225d05634acd582cdd800a5cc9aeddfb24cf8
jajajasalu2 <8iamthewalrus8@gmail.com>
2019-06-15 19:52:15 +0530

    Added domain priority, list of denied domains
    Used LxmlLinkExtractor for extracting links in the default parse method, which takes in the denied domains as an arguments. Added an important domains attribute to check for important domains and give these urls higher priority as per GCS's suggestion.

--- patchfinder/spiders/default_spider.py
+++ patchfinder/spiders/default_spider.py
@@ -1,10 +1,10 @@
-import scrapy
-from scrapy.http import Request
 from urllib.parse import urlparse
+from scrapy.http import Request
+from scrapy.linkextractors.lxmlhtml import LxmlLinkExtractor
+import scrapy
+from patchfinder.debian import DebianParser
 import patchfinder.spiders.items as items
 import patchfinder.entrypoint as entrypoint
-from patchfinder.debian import DebianParser
-import re
 
 class DefaultSpider(scrapy.Spider):
     """Scrapy Spider to extract patches
@@ -15,15 +15,24 @@ class DefaultSpider(scrapy.Spider):
     Attributes:
         name: Name of the spider
         recursion_limit: The recursion depth the spider would go to
-        entrypoints: A list of entrypoints the spider will crawl
         patches: A list of patch links the spider has found
+        deny_domains: A list of domains to deny crawling links of
+        important_domains: A list of domains with higher crawling priority
+        patch_limit: A threshold for the number of patches to collect
     """
+
+    deny_domains = ['facebook.com', 'twitter.com']
+    important_domains = []
+    patch_limit = 100
+    allowed_keys = {'deny_domains', 'important_domains', 'patch_limit'}
 
     def __init__(self, *args, **kwargs):
         self.name = 'default_spider'
         self.vuln_id = kwargs.get('vuln').vuln_id
         self.start_urls = kwargs.get('vuln').entrypoint_URLs
         self.patches = []
+        self.__dict__.update((k, v) for k, v in kwargs.items() \
+                             if k in self.allowed_keys)
         super(DefaultSpider, self).__init__(*args, **kwargs)
 
     def start_requests(self):
@@ -37,30 +46,34 @@ class DefaultSpider(scrapy.Spider):
         debian_parser = DebianParser()
         patches = debian_parser.parse(self.vuln_id)
         for patch in patches:
-            yield patch
+            if len(self.patches) < self.patch_limit:
+                self.add_patch(patch['patch_link'])
+                yield patch
 
     def parse(self, response):
         xpaths = entrypoint.get_xpath(response.url)
-        for xpath in xpaths:
-            links = response.xpath(xpath+'/@href').extract()
-            for link in links:
-                link = response.urljoin(link[0:])
-                if self.link_is_valid(link):
-                    patch_link = entrypoint.is_patch(link)
-                    if patch_link:
-                        if patch_link not in self.patches:
-                            patch = items.Patch()
-                            patch['patch_link'] = patch_link
-                            patch['reaching_path'] = response.url
-                            self.add_patch(patch_link)
-                            yield patch
-                    else:
-                        yield Request(link, callback=self.parse)
+        links = LxmlLinkExtractor(deny_domains=self.deny_domains,
+                                  restrict_xpaths=xpaths) \
+                                          .extract_links(response)
+        for link in links:
+            link = response.urljoin(link.url[0:])
+            patch_link = entrypoint.is_patch(link)
+            if patch_link and len(self.patches) < self.patch_limit:
+                if patch_link not in self.patches:
+                    patch = items.Patch()
+                    patch['patch_link'] = patch_link
+                    patch['reaching_path'] = response.url
+                    self.add_patch(patch_link)
+                    yield patch
+            elif len(self.patches) < self.patch_limit:
+                priority = self.domain_priority(link)
+                yield Request(link, callback=self.parse, priority=priority)
 
-    def link_is_valid(self, link):
-        if re.match(r'^http', link):
-            return True
-        return False
+    def domain_priority(self, url):
+        domain = urlparse(url).hostname
+        if domain in self.important_domains:
+            return 1
+        return 0
 
     def add_patch(self, patch_link):
         self.patches.append(patch_link)
