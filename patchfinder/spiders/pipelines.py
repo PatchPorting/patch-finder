0abc1bdb2d42582b67268818ee25cdd5d3ea1fcf
jajajasalu2 <jaskaransingh7654321@gmail.com>
2019-07-13 17:03:40 +0530

    Refactor spiders.pipelines to output JSON properly

--- patchfinder/spiders/pipelines.py
+++ patchfinder/spiders/pipelines.py
@@ -1,15 +1,18 @@
 import json
+from scrapy.exporters import JsonItemExporter
 import patchfinder.settings as settings
 
 
 class PatchPipeline(object):
     def open_spider(self, spider):
-        self.file = open(settings.PATCHES_JSON, "w")
+        self.file = open(settings.PATCHES_JSON, "wb")
+        self.exporter = JsonItemExporter(self.file)
+        self.exporter.start_exporting()
 
     def close_spider(self, spider):
+        self.exporter.finish_exporting()
         self.file.close()
 
     def process_item(self, item, spider):
-        line = json.dumps(dict(item)) + "\n"
-        self.file.write(line)
+        self.exporter.export_item(item)
         return item
