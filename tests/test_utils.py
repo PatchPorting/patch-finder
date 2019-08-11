728144347d9d5c788cefe73014528c9e256a6276
jajajasalu2 <jaskaransingh7654321@gmail.com>
2019-08-11 22:29:37 +0530

    Add docstrings for tests/test_utils.py

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -14,6 +14,7 @@ class TestUtils(unittest.TestCase):
 
     @mock.patch("patchfinder.utils.urllib.request.urlopen")
     def test_parse_web_page_offline_for_normal_xpaths(self, mock_urlopen):
+        """Strings from the normal xpaths should be scraped."""
         url = "https://security-tracker.debian.org/tracker/DSA-4444-1"
         try:
             f = open("./tests/mocks/debsec_dsa_4444_1.html")
@@ -35,6 +36,7 @@ class TestUtils(unittest.TestCase):
     @mock.patch("patchfinder.utils.urllib.request")
     @mock.patch("patchfinder.utils.os")
     def test_download_item_file_exists(self, mock_os, mock_urllib_request):
+        """Item should not be downloaded as it exists and overwrite is False."""
         file_name = "./tests/mocks/mock_file"
         file_url = "mock_url"
         mock_os.path.isfile.return_value = True
@@ -46,6 +48,7 @@ class TestUtils(unittest.TestCase):
     @mock.patch("patchfinder.utils.urllib.request")
     @mock.patch("patchfinder.utils.os")
     def test_download_item_file_not_exists(self, mock_os, mock_urllib_request):
+        """Item should be downloaded as it does not exists."""
         file_name = "./tests/mocks/mock_file"
         file_url = "mock_url"
         mock_os.path.isfile.return_value = False
@@ -57,6 +60,7 @@ class TestUtils(unittest.TestCase):
         mock_urllib_request.urlretrieve.assert_called_with(file_url, file_name)
 
     def test_member_in_tarfile(self):
+        """Members present should be found and absent should not be found."""
         tar_file = "./tests/mocks/openjpeg2_2.1.1-1.debian.tar.xz"
         self.assertTrue(member_in_tarfile(tar_file, "debian"))
         self.assertFalse(member_in_tarfile(tar_file, "deb"))
