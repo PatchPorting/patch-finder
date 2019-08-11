6767a8cd967ffb2ad8a3cf2d0926efd49eb6ce2f
jajajasalu2 <jaskaransingh7654321@gmail.com>
2019-08-11 14:55:53 +0530

    Add test for DebianParser
    No debian package URL found on snapshot.d.o.

--- tests/test_debian_parse.py
+++ tests/test_debian_parse.py
@@ -28,7 +28,7 @@ class TestDebianParser(unittest.TestCase):
         patches = self.parser.parse(vuln_id)
         mock_parse_page.assert_called_with(
             "https://snapshot.debian.org/package/openjpeg2/2.1.1-1/",
-            xpaths=["//a/@href[contains(., 'openjpeg2_2.1.1-1.debian')]"]
+            xpaths=["//a/@href[contains(., 'openjpeg2_2.1.1-1.debian')]"],
         )
         self.assertFalse(patches)
 
@@ -37,6 +37,11 @@ class TestDebianParser(unittest.TestCase):
     def test_debian_parser_with_patches_in_package(
         self, mock_download_item, mock_parse_page
     ):
+        """Patches should be found in the package.
+
+        Tests:
+            patchfinder.parsers.debian_parser.DebianParser.parse
+        """
         vuln_id = "CVE-2018-20544"
         mock_parse_page.side_effect = [
             ["libcaca", "0.99.beta19-2.1"],
@@ -49,7 +54,7 @@ class TestDebianParser(unittest.TestCase):
         patches = self.parser.parse(vuln_id)
         mock_parse_page.assert_called_with(
             "https://snapshot.debian.org/package/libcaca/0.99.beta19-2.1/",
-            xpaths=["//a/@href[contains(., 'libcaca_0.99.beta19-2.1.debian')]"]
+            xpaths=["//a/@href[contains(., 'libcaca_0.99.beta19-2.1.debian')]"],
         )
         self.assertIn(
             {
@@ -62,6 +67,33 @@ class TestDebianParser(unittest.TestCase):
             },
             patches,
         )
+
+    @mock.patch("patchfinder.parsers.debian_parser.utils.parse_web_page")
+    @mock.patch("patchfinder.parsers.debian_parser.utils.download_item")
+    def test_debian_parse_with_no_debian_tarball_found(
+        self, mock_download_item, mock_parse_page
+    ):
+        """No debian tarball found on snapshot.d.o, so no patches found.
+
+        Tests:
+            patchfinder.parsers.debian_parser.DebianParser.parse
+        """
+        vuln_id = "CVE-2018-20406"
+        mock_parse_page.side_effect = [
+            ["python3.4", "(unfixed)", "python3.4", "3.4.2-1+deb8u2"],
+            [],
+        ]
+        patches = self.parser.parse(vuln_id)
+        mock_parse_page.assert_called_with(
+            "https://snapshot.debian.org/package/python3.4/3.4.2-1+deb8u2/",
+            xpaths=[
+                "//a/@href[contains(., 'python3.4_3.4.2-1%2Bdeb8u2.debian')]"
+            ],
+        )
+
+        # No packages were downloaded.
+        mock_download_item.assert_not_called()
+        self.assertFalse(patches)
 
 
 if __name__ == "__main__":
